90ef2ae06184d4faa44d334a0c605b6afda77d97
Elliott-Roach <elliott.roach@mths.ca>
2024-10-24 10:46:42 -0400

    takes distance and shows red if to close and grean if good

--- main.ts
+++ main.ts
@@ -5,4 +5,43 @@
  * This program senses distance and terns red if to close
 */
 
+let distanceToObject: number = 0
+let neopixleStrip: neopixel.Strip = null
+
+//clean
+basic.clearScreen()
+neopixleStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixleStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixleStrip.show()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P15,
+        DigitalPin.P14,
+        PingUnit.Centimeters,
+    )
+
+    if (distanceToObject > 10) {
+        neopixleStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+        neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixleStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        neopixleStrip.show()
+    } else {
+        neopixleStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+        neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        neopixleStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+        neopixleStrip.show()
+    }
+
+
+})
 
