0004a265156b1bc6afc85bfb03061f8245c615a2
Elliott-Roach <elliott.roach@mths.ca>
2024-10-24 10:59:26 -0400

    fix if statment

--- main.ts
+++ main.ts
@@ -20,13 +20,17 @@ basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
+
+    //taking distance
     distanceToObject = sonar.ping(
         DigitalPin.P15,
         DigitalPin.P14,
         PingUnit.Centimeters,
     )
 
-    if (distanceToObject > 10) {
+    //terning red if 9 cm or less and green if 10 or more
+    basic.showNumber(distanceToObject)
+    if (distanceToObject < 10) {
         neopixleStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
         neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
         neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
@@ -41,7 +45,5 @@ input.onButtonPressed(Button.A, function () {
         neopixleStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
         neopixleStrip.show()
     }
-
-
+    basic.showIcon(IconNames.Happy)
 })
-
